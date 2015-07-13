5917b6a04f2af104e454a314134baf3f519f160f
张胜东 <435878393@qq.com>
2015-07-14 01:25:53 +0800

    完成派生类CNewSocket

--- 服务器端/NewSocket.cpp
+++ 服务器端/NewSocket.cpp
@@ -4,9 +4,35 @@
 
 CNewSocket::CNewSocket()
 {
+	my_nLength = 0;
+	memset(my_szBuffer, 0, sizeof(my_szBuffer));
 }
 
 
 CNewSocket::~CNewSocket()
 {
+	if (m_hSocket != INVALID_SOCKET) Close();
+}
+
+
+void CNewSocket::OnReceive(int nErrorCode)
+{
+	// TODO:  在此添加专用代码和/或调用基类
+	my_nLength = Receive(my_szBuffer, sizeof(my_szBuffer), 0);		//接收消息
+	AsyncSelect(FD_WRITE);											//触发OnSend()函数,(转发接收到的消息)
+
+	CAsyncSocket::OnReceive(nErrorCode);
+}
+
+
+void CNewSocket::OnSend(int nErrorCode)
+{
+	// TODO:  在此添加专用代码和/或调用基类
+	char my_SendBuffer[4096];
+	memcpy(my_SendBuffer, "服务器转发:", 24);
+	strncat(my_SendBuffer, my_szBuffer, my_nLength);				//复制赋值
+	Send(my_SendBuffer, sizeof(my_SendBuffer));						//发送数据
+	AsyncSelect(FD_READ);											//触发OnReceive()函数
+
+	CAsyncSocket::OnSend(nErrorCode);
 }
--- 服务器端/NewSocket.h
+++ 服务器端/NewSocket.h
@@ -6,5 +6,10 @@ class CNewSocket :
 public:
 	CNewSocket();
 	~CNewSocket();
+
+	UINT my_nLength;
+	char my_szBuffer[4096];
+	virtual void OnReceive(int nErrorCode);
+	virtual void OnSend(int nErrorCode);
 };
 
